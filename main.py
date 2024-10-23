5a69817471d594d91ba481aadb954618a7f40ff2
Matt Taylor <62269525+MrMatt1010@users.noreply.github.com>
2024-10-23 19:10:03 +1300

    Update main.py
    Quick correction to canvas size

--- main.py
+++ main.py
@@ -128,10 +128,10 @@ def evaluate_and_update_strength():
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
-window.title("Password Manager")
+window.title("M and S Password Manager")
 window.config(padx=50, pady=50)
 
-canvas = Canvas(height=200, width=200)
+canvas = Canvas(height=217, width=217)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
